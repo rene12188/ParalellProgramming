d0a8bd945817bd071d40b6670c87898678a79670
Rene Sorger <rene_sorger@gmx.at>
2023-02-17 22:05:46 +0100

    Startup Created

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+
+var n = int.Parse(Environment.GetCommandLineArgs()[1]);
+var thinkingTime = int.Parse(Environment.GetCommandLineArgs()[2]);
+var eatingTime = int.Parse(Environment.GetCommandLineArgs()[3]);
+
+Console.WriteLine(n);
+Console.WriteLine(thinkingTime);
+Console.WriteLine(eatingTime);
