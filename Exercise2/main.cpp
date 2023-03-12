fd3a8ffcd97627698100893ee547b8af2de3f12c
Grengster <gregor.zeman@gmx.at>
2023-03-12 16:45:42 +0100

    Finished zoom

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -9,10 +9,10 @@ const int WIDTH = 1000;
 const int HEIGHT = 1000;
 const int MAX_ITERATIONS = 1000;
 
-const double xmin = -2.0;
-const double xmax = 1.0;
-const double ymin = -1.0;
-const double ymax = 1.0;
+const double xmin = -10.0;
+const double xmax = 5.0;
+const double ymin = -5.0;
+const double ymax = 5.0;
 
 std::pair<double, double> mapPixel(int x, int y)
 {
@@ -37,82 +37,95 @@ int main()
 	texture.loadFromImage(image);
 	sf::Sprite sprite(texture);
 
-	// Main loop
-	while (window.isOpen())
-	{
-		// Handle events
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-			{
-				window.close();
-			}
-		}
+    // Define the initial zoom level and zoom factor
+    double zoom = 1;
+    const double zoomFactor = 0.99;
 
-		auto start = std::chrono::high_resolution_clock::now();
+
+    // Main loop
+    while (window.isOpen())
+    {
+        // Handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+
+        const double VIEW_SHIFT = -0.2 * (xmax - xmin);
+        // Zoom into the set
+        zoom *= zoomFactor;
+        double xmin_zoomed = (xmin + VIEW_SHIFT) * zoom;
+        double xmax_zoomed = (xmax + VIEW_SHIFT) * zoom;
+        double ymin_zoomed = ymin * zoom;
+        double ymax_zoomed = ymax * zoom;
+
 #pragma omp parallel
-		{
+        {
 #pragma omp for
-		// Draw the Mandelbrot set
-		for (int x = 0; x < WIDTH; ++x)
-		{
-				for (int y = 0; y < HEIGHT; ++y)
-				{
-					auto mappedPixel = mapPixel(x, y);
-					double real = mappedPixel.first;
-					double imag = mappedPixel.second;
+            // Draw the Mandelbrot set
+            for (int x = 0; x < WIDTH; ++x)
+            {
+                for (int y = 0; y < HEIGHT; ++y)
+                {
+                    auto mappedPixel = mapPixel(x, y);
+                    double real = mappedPixel.first;
+                    double imag = mappedPixel.second;
 
-					// Initialize the complex number
-					double z_real = real;
-					double z_imag = imag;
+                    // Map the pixel to the zoomed coordinate system
+                    double mappedX = xmin_zoomed + (xmax_zoomed - xmin_zoomed) * x / (WIDTH - 1);
+                    double mappedY = ymin_zoomed + (ymax_zoomed - ymin_zoomed) * y / (HEIGHT - 1);
 
-					// Iterate the complex number until it escapes or max iterations are reached
-					int iterations = 0;
-					while (z_real * z_real + z_imag * z_imag <= 4 && iterations < MAX_ITERATIONS)
-					{
-						double z_real_new = z_real * z_real - z_imag * z_imag + real;
-						double z_imag_new = 2 * z_real * z_imag + imag;
-						z_real = z_real_new;
-						z_imag = z_imag_new;
-						++iterations;
-					}
+                    // Initialize the complex number
+                    double z_real = mappedX;
+                    double z_imag = mappedY;
 
-					// Set the pixel color based on the number of iterations
-					if (iterations == MAX_ITERATIONS)
-					{
-						image.setPixel(x, y, sf::Color::Black);
-					}
-					else
-					{
-						float t = (float)iterations / MAX_ITERATIONS;
-						int r = (int)(9 * (1 - t) * t * t * t * 255);
-						int g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
-						int b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
-						image.setPixel(x, y, sf::Color(r, g, b));
-					}
-					// Update the texture and sprite
+                    // Iterate the complex number until it escapes or max iterations are reached
+                    int iterations = 0;
+                    while (z_real * z_real + z_imag * z_imag <= 4 && iterations < MAX_ITERATIONS)
+                    {
+                        double z_real_new = z_real * z_real - z_imag * z_imag + mappedX;
+                        double z_imag_new = 2 * z_real * z_imag + mappedY;
+                        z_real = z_real_new;
+                        z_imag = z_imag_new;
+                        ++iterations;
+                    }
 
-					// _sleep(10);
-					//texture.update(image);
-					//window.draw(sprite);
-					//window.display();
-				}
-				
-			}
+                    // Set the pixel color based on the number of iterations
+                    if (iterations == MAX_ITERATIONS)
+                    {
+                        image.setPixel(x, y, sf::Color::Black);
+                    }
+                    else
+                    {
+                        float t = (float)iterations / MAX_ITERATIONS;
+                        int r = (int)(9 * (1 - t) * t * t * t * 255);
+                        int g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
+                        int b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
+                        image.setPixel(x, y, sf::Color(r, g, b));
+                    }
+                }
+            }
+            _sleep(5);
+        }
 
-		}
+        auto end = std::chrono::high_resolution_clock::now();
 
-		auto end = std::chrono::high_resolution_clock::now();
+        auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-		auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+        // Update the texture and sprite
+        texture.update(image);
+        window.draw(sprite);
+        window.display();
 
-		texture.update(image);
-		window.draw(sprite);
-		window.display();
-
-		std::cout << "elapsed milliseconds: " << milliseconds.count() << std::endl;
-		break;
+        std::cout << "elapsed milliseconds: " << milliseconds.count() << std::endl;
+		//break;
 	}
 
 	return 0;
