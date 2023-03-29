7af71fe81357bfd207de62317248e37baa470cc4
Rene Sorger <rene_sorger@gmx.at>
2023-03-29 23:49:37 +0200

    Fixed Exporter

--- Project.PowerBalancer/Modules/Reporter/JsonReporter.cs
+++ Project.PowerBalancer/Modules/Reporter/JsonReporter.cs
@@ -1,66 +1,51 @@
 ﻿using Newtonsoft.Json;
 using Project.PowerBalancer.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Project.PowerBalancer.Modules.Reporter
+namespace Project.PowerBalancer.Modules.Reporter;
+
+public class JsonReporter : IReporter
 {
-    public class JsonReporter : IReporter
+    private List<Record> records = new();
+
+    public void Report(IList<Community> communities)
     {
-        List<Record> records = new List<Record>();
-
-        public void Report(IList<Community> communities)
+        foreach (var item in communities)
         {
-            foreach (var item in communities)
-            {
-                var newRecord = new Record();
-                newRecord.CommunityName = item.Name;
-                //newRecord.time = item.
+            var newRecord = new Record();
+            newRecord.CommunityName = item.Name;
 
-                foreach (var boughtReceipt in item.PowerBoughtReport)
+            foreach (var boughtReceipt in item.PowerBoughtReport)
+                newRecord.bought.Add(new TransactionRecord
                 {
-                    newRecord.bought.Add(new TransactionRecord()
-                    {
-                        name = boughtReceipt.Item1, amount = boughtReceipt.Item2
-                    });
-                }
+                    name = boughtReceipt.Item1, amount = boughtReceipt.Item2
+                });
 
-                foreach (var soldReceipt in item.PowerSoldReport)
+            foreach (var soldReceipt in item.PowerSoldReport)
+                newRecord.sold.Add(new TransactionRecord
                 {
-                    newRecord.sold.Add(new TransactionRecord()
-                    {
-                        name = soldReceipt.Item1, amount = soldReceipt.Item2
-                    });
-                }
-                
-                //Console.WriteLine("\n\n" + newRecord + "\n\n");
-                records.Add(newRecord);
-            }
-        }
-
-        public void FlushReport(IList<Community> communities)
-        {
-            Report(communities);
-
-            ExportToJson(records, "./records.json");
-        }
-
-        public static void ExportToJson(List<Record> records, string filePath)
-        {
-            try
-            {
-                var json = JsonConvert.SerializeObject(records);
-                File.WriteAllText(filePath, json);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error exporting to JSON file: {ex.Message}");
-            }
+                    name = soldReceipt.Item1, amount = soldReceipt.Item2
+                });
+            records.Add(newRecord);
         }
     }
 
-   
+    public void FlushReport(IList<Community> communities)
+    {
+        Report(communities);
+
+        ExportToJson(records, "./records.json");
+    }
+
+    public void ExportToJson(List<Record> records, string filePath)
+    {
+        try
+        {
+            var json = JsonConvert.SerializeObject(records);
+            File.WriteAllText(filePath, json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error exporting to JSON file: {ex.Message}");
+        }
+    }
 }
